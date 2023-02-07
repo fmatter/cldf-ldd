04206ac625c32f2237aaf523c2bc31b84ac2e427
Florian Matter <florianmatter@gmail.com>
2023-02-07 08:24:50 +0100

    foreign key insertion

--- src/cldf_ldd/__init__.py
+++ src/cldf_ldd/__init__.py
@@ -13,9 +13,6 @@ __email__ = "fmatter@mailbox.org"
 __version__ = "0.0.1.dev"
 
 
-__all__ = ["MorphTable", "POSTable"]
-
-
 def valid_parts(name, dataset, table, column, row):
     value = row[column.name]
     if value and None in value:
@@ -33,3 +30,12 @@ validators = [
 
 def validate(ds):
     ds.validate(validators=validators)
+
+
+def add_keys(ds):
+    cldf_tables = list(ds.components.keys()) + [
+        str(x.url) for x in ds.tables
+    ]  # a list of tables in the dataset
+    for src, key1, goal, key2 in keys:
+        if src in cldf_tables and goal in cldf_tables:
+            ds.add_foreign_key(src, key1, goal, key2)
