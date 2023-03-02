4b346b8725f2877739c124300b702b4dfa23e71a
Florian Matter <florianmatter@gmail.com>
2023-03-02 09:38:23 -0800

    phonemes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+* PhonemeTable
+
 ## [0.0.4] - 2023-03-01
 
 ### Changed
--- etc/check_components.py
+++ etc/check_components.py
@@ -17,8 +17,6 @@ for handle, name in filenames.items():
     desc_path = handle_path / "description.md"
     if not md_path.is_file():
         print("does not exist:", md_path)
-        # with open(md_path, 'w') as json_file:
-        # json.dump(default_json(handle), json_file, indent=4)
     if not desc_path.is_file():
         print("Creating", desc_path)
         with open(desc_path, "w") as f:
